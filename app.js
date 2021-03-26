5f1f7e8d798b5f54b56807aa3d116a1ac0ee5d87
Anvith KS <anvith3@gmail.com>
2021-03-26 15:08:07 +0530

    Updated the collection and the runner

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var newman = require('newman');
 
 // call newman.run to pass `options` object and wait for callback
 newman.run({
-    collection: require('./SODA_Multicloud_Automation_Test.postman_collection.json'),
+    collection: require('./SODA_Multicloud_Automation_Test_with_invalid_backends.postman_collection.json'),
     globals: require('./SODA.postman_globals.json'),
     //exportGlobals: ('./soda_global_vars.json'),
     iterationData: ('./iteration_data.json'),
@@ -11,7 +11,7 @@ newman.run({
     //bail: true,
     reporter: {
         htmlextra: {
-            export: './reports/soda_multicloud_e2e_report.html',
+            //export: './reports/soda_multicloud_e2e_report.html',
             // template: './template.hbs'
             logs: true,
             // showOnlyFails: true,
@@ -21,13 +21,13 @@ newman.run({
             title: "SODA Multicloud E2E Test report",
             // titleSize: 4,
             // omitHeaders: true,
-            skipHeaders: "X-Auth-Token",
-            hideRequestBody: ["Register Backend", "Register Backend Invalid Credentials"],
-            hideResponseBody: ["Register Backend", "Register Backend Invalid Credentials"],
+            //skipHeaders: "X-Auth-Token",
+           // hideRequestBody: ["Register Backend", "Register Backend Invalid Credentials"],
+            //hideResponseBody: ["Register Backend", "Register Backend Invalid Credentials"],
             // showEnvironmentData: true,
             // skipEnvironmentVars: ["API_KEY"],
             // showGlobalData: true,
-            skipGlobalVars: ["authToken"],
+            //skipGlobalVars: ["authToken"],
             // skipSensitiveData: true,
             // showMarkdownLinks: true,
             // showFolderDescription: true,
@@ -36,12 +36,13 @@ newman.run({
     },
     insecure: true, // allow self-signed certs, required in postman too,
     delayRequest: 2000,
-    timeout: 180000  // set time out,
+    //timeout: 180000  // set time out,
 }).on('start', function (err, args) { // on start of run, log to console
     console.log('Running E2E tests for SODA multicloud ...');
 }).on('done', function (err, summary) {
     if (err || summary.error) {
-        console.error('collection run encountered an error.');
+        console.error('collection run encountered an error.', err);
+        console.log("Summary Error",summary.error);
     }
     else {
         console.log('collection run completed.');
